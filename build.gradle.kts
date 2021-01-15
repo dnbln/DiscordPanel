881e7265cd2d7b2b942aa9df966168760d7c5da7
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2021-01-15 14:27:11 +0200

    Finally load in 2020.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("jvm") version "1.3.72"
-    id("org.jetbrains.intellij") version "0.6.4"
+    id("org.jetbrains.intellij") version "0.6.5"
 }
 
 
 group = "org.dblanovschi.DiscordPanel"
-version = "0.1.3"
+version = "0.1.4"
 
 repositories {
     mavenCentral()
@@ -18,18 +18,26 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "2020.2.3"
+    version = "2020.3.1"
 }
 
 tasks {
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions {
+            jvmTarget = "11"
+            languageVersion = "1.3"
+            apiVersion = "1.3"
+        }
     }
     patchPluginXml {
+        sinceBuild("202.6397.94")
         changeNotes(
         """
         - Allow plugin to be run in 2020.3(203.* builds) <br/>
-        - Add a way to reuse previous sessions (<a href="https://github.com/dblanovschi/DiscordPanel/issues/1">#1</a>) <br/>
         """.trimMargin())
+    }
+
+    runPluginVerifier {
+        setIdeVersions(listOf("IIC-2020.2.4", "IIC-2020.3.1"))
     }
 }
